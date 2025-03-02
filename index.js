80ee2c7469dbabe1e9504cd7115e7166a35fe3d6
AlexKi07 <kimanialexk07@gmail.com>
2025-03-02 23:32:25 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+   const apendCat = [...cats, "Broom"]
+   return apendCat
+}
+
+function prependCat(name) {
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+
+function removeLastCat() {
+   return cats.slice(0, 2, "")
+}
+
+function removeFirstCat() {
+    return cats.slice(1, 3)
+ }
+
